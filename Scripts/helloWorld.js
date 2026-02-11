34404a5b959a6372db770af9e69e6efe45a52fdc
dherrera-code <dherr929@gmail.com>
2026-02-11 11:59:26 -0800

    need to resolve CORS policy!

--- Scripts/helloWorld.js
+++ Scripts/helloWorld.js
@@ -6,7 +6,12 @@ const enterBtn = document.getElementById("enterBtn")
 const resetBtn = document.getElementById("resetBtn");
 
 
-
+const getHelloData = async (name) => {
+    const response = await fetch(`https://allforoneapidh-h2cygwchdcbbcsce.westus3-01.azurewebsites.net/api/helloworld/hello/${name}`);
+    const data = await response.text();
+    console.log(data)
+    return data;
+}
 
 
 //add btn event listeners!
