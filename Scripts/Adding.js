992a3093e4d6867d685d6a0814bb211c9313227b
dherrera-code <dherr929@gmail.com>
2026-02-11 15:29:33 -0800

    AddingTwoNumbers is now fully functional!

--- Scripts/Adding.js
+++ Scripts/Adding.js
@@ -1,1 +1,24 @@
-// alert("plz work");
+const apiurl = "https://allforoneapidh-h2cygwchdcbbcsce.westus3-01.azurewebsites.net"
+const inputNum1 = document.getElementById("inputNum1");
+const inputNum2 = document.getElementById("inputNum2");
+const enterBtn = document.getElementById('enterBtn');
+
+const displaySum = document.getElementById("displaySum");
+
+const resetBtn = document.getElementById("resetBtn");
+
+const getSum = async (num1, num2) => {
+    const response = await fetch(apiurl + `/api/addingtwonumber/adding/${num1}/${num2}`);
+    const data = await response.text();
+    return data;
+}
+
+enterBtn.addEventListener("click", async () => {
+    let sum = await getSum(inputNum1.value, inputNum2.value);  
+    displaySum.textContent = sum;
+})
+resetBtn.addEventListener("click", () => {
+    inputNum1.value = "";
+    inputNum2.value = "";
+    displaySum.textContent ="";
+})
--- Scripts/helloWorld.js
+++ Scripts/helloWorld.js
@@ -1,4 +1,4 @@
-
+const apiurl = "https://allforoneapidh-h2cygwchdcbbcsce.westus3-01.azurewebsites.net";
 const displayHello = document.getElementById("displayHelloUser");
 const inputName = document.getElementById("inputName")
 const enterBtn = document.getElementById("enterBtn")
@@ -6,7 +6,7 @@ const resetBtn = document.getElementById("resetBtn");
 
 
 const getHelloData = async (name) => {
-    const response = await fetch(`https://allforoneapidh-h2cygwchdcbbcsce.westus3-01.azurewebsites.net/api/helloworld/hello/${name}`);
+    const response = await fetch( apiurl +`/api/helloworld/hello/${name}`);
     const data = await response.text();
     console.log(data)
     // return data;
